f339c0de9ae33d678165e10b62dbab3d920f678d
MSempere <msempere@gmx.com>
2016-03-29 12:10:05 +0100

    Bash script for uploading files to S3 using awscli

---
+++ 2-collectors/clojure-collector/java-servlet/war-resources/.ebextensions/upload_log_to_s3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FILES=/var/log/tomcat8/rotated/*
+instanceid=$(wget -q -O - http://instance-data/latest/meta-data/instance-id)
+regex=".*\.txt-([0-9]*)-([0-9]*)-([0-9]*)-([0-9]*)\.lzo"
+version="test"
+bucket="yieldify-aa-stream-staging-dev"
+
+for f in $FILES
+do
+    if [[ "$f" =~ $regex ]]
+    then
+        echo "Processing $f correctly"
+        timestamp="${BASH_REMATCH[4]}"
+        date_path=$(date -d @$timestamp +"%Y/%m/%d/%Y-%m-%d-%H")
+        path=$bucket/$version/${date_path}_${instanceid}_${timestamp}.lzo
+        count=`aws s3 ls s3://$path | wc -l`
+
+        if [[ $count -gt 0 ]]; then
+            echo "s3://$path already exists. Not uploading"
+        else
+            aws s3 cp $f s3://$path
+        fi
+    else
+        echo "$f doesnt match regex"
+    fi
+done
+
