e7c30e7502e1a8e62fe29e1e40d25fba6f00187d
iasonb <iason@yieldify.com>
2016-07-18 13:01:58 +0100

    Prepare tag v1 collector for deployment in production env

--- 2-collectors/clojure-collector/java-servlet/war-resources/.ebextensions/upload_log_to_s3.sh
+++ 2-collectors/clojure-collector/java-servlet/war-resources/.ebextensions/upload_log_to_s3.sh
@@ -4,6 +4,7 @@ FILES=/var/log/tomcat8/rotated/*
 instanceid=$(/opt/aws/bin/ec2-metadata --instance-id | cut -d " " -f 2)
 regex=".*\.txt-([0-9]*)-([0-9]*)-([0-9]*)-([0-9]*)\.lzo"
 version="v1"
+tagversion="tag-v1"
 bucket="yieldify-aa-stream-production"
 logtype="raw"
 
@@ -14,7 +15,7 @@ do
         echo "Processing $f correctly"
         timestamp="${BASH_REMATCH[4]}"
         date_path=$(date -d @$timestamp +"%Y/%m/%d/%Y-%m-%d-%H")
-        path=$bucket/$version/${logtype}/${date_path}_${instanceid}_${timestamp}.lzo
+        path=$bucket/$version/$tagversion/${logtype}/${date_path}_${instanceid}_${timestamp}.lzo
         count=`aws s3 ls s3://$path | wc -l`
 
         if [[ $count -gt 0 ]]; then
